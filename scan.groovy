e2a313acda322b137defc6b74ad7531e6cad382f
bansodesatish <84638432+bansodesatish@users.noreply.github.com>
2023-01-16 12:04:50 +0530

    Create scan.groovy

---
+++ scan.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent {
+        kubernetes {
+            label 'kube-agent'
+            yaml '''
+            apiVersion: v1
+            kind: Pod
+            spec:
+                serviceAccountName: jenkins-admin
+                containers:
+                - name: chaos-builder
+                  image: jenkinsxio/builder-base:0.1.275
+                  command:
+                  - cat
+                  tty: true
+                  volumeMounts:
+                  - name: docker
+                    mountPath: /var/run/docker.sock
+                volumes:
+                - name: docker
+                  hostPath:
+                    path: /var/run/docker.sock
+            '''
+        }
+    }
+
+        stage('Build image and push it to dev') {
+            steps {
+                container('chaos-builder') {
+                    sh '''
+                    echo "pipeline working...."
+                    '''
+                    
+                }  
+            }
+        }
+}
