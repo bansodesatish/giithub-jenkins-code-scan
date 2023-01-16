9122d080a3045980b83dd7a1e428eff1e32f213c
satish bansode <bansode.m.satish@gmail.com>
2023-01-16 19:02:02 +0530

    updated lambda function

--- lambda/pokemon/lambda_funtion.py
+++ lambda/pokemon/lambda_funtion.py
@@ -1,28 +1,7 @@
-import json
-import requests
-
+import numpy as np
+import pandas as pd
 def lambda_handler(event, context):
-    # Make sure a Pokemon name (or ID) was passed in the event object:
-    pokemon = event.get('pokemon')
-    if pokemon is None:
-        return {
-            'statusCode': 400,
-            'body': json.dumps('Missing pokemon attribute in the event object')
-        }
-
-    # If we have a pokemon name/ID passed in, try to get info for it:
-    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-
-    if r.status_code == 200:
-        status_code = 200
-        body = json.dumps(r.json())
-    else:
-        status_code = r.status_code
-        body = json.dumps(f"Could not load information for Pokemon {pokemon}")
-
-    # Return what we got from the API:
-    return {
-        'statusCode': status_code,
-        'body': body
-    }
-    
+    df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),columns=["a", "b", "c"])
+    number = np.pi
+    print(df2)
+    print(number)
