0c8d19469f4411b0fafd9b48022df729f2eeaf09
satish bansode <bansode.m.satish@gmail.com>
2023-01-17 19:35:32 +0530

    updated docker image

--- scan.groovy
+++ scan.groovy
@@ -17,7 +17,7 @@ pipeline {
                   - name: docker
                     mountPath: /var/run/docker.sock
                 - name: blackduck
-                  image: bansodesatish/blackduck:python
+                  image: bansodesatish/blackduck:1
                   cammand: 
                   - cat
                   tty: true
