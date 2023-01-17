ae6f61458d194d06c5cdf92d76e585e9cc248a10
satish bansode <bansode.m.satish@gmail.com>
2023-01-17 13:44:53 +0530

    updated lambdas and jenkins script

--- scan.groovy
+++ scan.groovy
@@ -28,6 +28,14 @@ pipeline {
             '''
         }
     }
+    environment {
+        BLACKDUCK_PROJECT_APPLICATION_NAME="123489"
+        BLACKDUCK_PROJECT_VERSION_NAME=${BUILD_NUBMER}
+        BLACKDUCK_PROJECT_TAGS="internal"
+        BLACKDUCK_SOURCE_PATH="${WORKSPACE}/lambdas/pokemon"
+        BLACKDUCK_SEARCH_DEPTH=0
+        BLACKDUCK_PIP_REQUIREMENTS_PATH="${WORKSPACE}/lambdas/requirements.txt"
+    }
     stages {
         stage('Build image and push it to dev') {
             steps {
@@ -37,6 +45,7 @@ pipeline {
                     which python3
                     which pip3
                     which pipenv
+                    python3 -m pip install -r $env.BLACKDUCK_PIP_REQUIREMENTS_PATH
                     '''
                     script{
                         env.PYTHON_3_PATH=sh (returnStdout: true, script: 'which python3').trim()
@@ -44,20 +53,21 @@ pipeline {
                     }
                     synopsys_detect detectProperties: '''
                     --blackduck.offline.mode=true \
-                    --detect.project.application.id=462841 \
-                    --detect.project.version.name="${BUILD_NUMBER}" \
-                    --detect.project.tags=internal,contractores,notdistributed,bottomzone,middlezone,nonmodifiedoss,mexux \
-                    --detect.source.path="${WORKSPACE}/lambda/pokemon" \
-                    --detect.detector.search.depth=0 --detect.python.python3=true \
+                    --detect.project.application.id=${env.BLACKDUCK_PROJECT_APPLICATION_NAME} \
+                    --detect.project.version.name="${env.BLACKDUCK_PROJECT_VERSION_NAME}" \
+                    --detect.project.tags=${env.BLACKDUCK_PROJECT_TAGS} \
+                    --detect.source.path="${env.BLACKDUCK_SOURCE_PATH}" \
+                    --detect.detector.search.depth=${env.BLACKDUCK_SEARCH_DEPTH} \
+                    --detect.python.python3=true \
                     --detect.python.path=${PYTHON_3_PATH} \
                     --detect.pip.path=${PIP_3_PATH}  \
-                    --detect.pip.requirements.path="${WORKSPACE}/requirements.txt" \
+                    --detect.pip.requirements.path="${env.BLACKDUCK_PIP_REQUIREMENTS_PATH}" \
                     --detect.tools.excluded="SIGNATURE_SCAN" \
                     --logging.level.detect=TRACE  \
-                    --detect.cleanup=true \
                     --logging.level.com.synopsys.integration=TRACE \
                     --detect.bdio.output.path="${WORKSPACE}" \
-                    ---detect.bom.aggregate.name="${BUILD_NUMBER}_bom" \
+                    --detect.bom.aggregate.name="${BUILD_NUMBER}_bom" \
+                    --detect.cleanup=true \
                     ''', downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
                     
                 }  
